20a2450eab8c4f2526741f3d908640c7307495e8
Alexandre CAROL <sasandre10@gmail.com>
2025-07-07 23:50:31 +0200

    Fix: Register

--- Backend/Identity/Services/UserService.cs
+++ Backend/Identity/Services/UserService.cs
@@ -74,7 +74,7 @@ namespace Identity.Services
         {
             if (await _userManager.FindByEmailAsync(request.Email) != null)
             {
-                return Result<User>.Fail("Registration failed. Please verify your details and try again");
+                return Result<User>.Fail("This email is already registered. Please use a different email.");
             }
 
             User user = new User
@@ -89,16 +89,20 @@ namespace Identity.Services
 
             user.PasswordHash = _passwordManager.HashPassword(user, request.Password);
 
-            await _userManager.CreateAsync(user, request.Password);
+            IdentityResult identityResult = await _userManager.CreateAsync(user, request.Password);
 
-            await _userManager.AddToRoleAsync(user, "Admin"); // Temporaire
-
-            IdentityResult identityResult = await _userManager.CreateAsync(user);
+            if (!identityResult.Succeeded)
+            {
+                return Result<User>.Fail($"User creation errors: {string.Join(", ", identityResult.Errors.Select(e => e.Description))}");
+            }
 
             if (identityResult.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Admin"); // Temporaire
                 return Result<User>.Ok(user);
+            }
 
-             return Result<User>.Fail("Registration failed. Please verify your details and try again");
+            return Result<User>.Fail("Registration failed. Please verify your details and try again");
         }
     }
 }
